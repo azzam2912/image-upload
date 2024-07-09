ea18f980caae33c2b29bf2ac09cc52715bb7b527
azzam2912 <azzam.gamers12@gmail.com>
2024-07-09 11:47:24 +0700

    add get request

--- main.go
+++ main.go
@@ -1,59 +1,63 @@
 package main
-import (
-    "fmt"
-    "log"
-    "strings"
 
-    "github.com/gofiber/fiber/v2"
-    "github.com/google/uuid"
+import (
+	"fmt"
+	"log"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
 )
 
 func uploadImage(c *fiber.Ctx) error {
- file, err := c.FormFile("image")
+	file, err := c.FormFile("image")
 
-  if err != nil {
-   log.Println("Error in uploading Image : ", err)
-   return c.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
- 
-  }
- 
-  uniqueId := uuid.New()
- 
-  filename := strings.Replace(uniqueId.String(), "-", "", -1)
- 
-  fileExt := strings.Split(file.Filename, ".")[1]
- 
-  image := fmt.Sprintf("%s.%s", filename, fileExt)
- 
-  err = c.SaveFile(file, fmt.Sprintf("./images/%s", image))
- 
-  if err != nil {
-   log.Println("Error in saving Image :", err)
-   return c.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
-  }
- 
-  imageUrl := fmt.Sprintf("http://localhost:3000/images/%s", image)
- 
-  data := map[string]interface{}{
- 
-   "imageName": image,
-   "imageUrl":  imageUrl,
-   "header":    file.Header,
-   "size":      file.Size,
-  }
- 
-  return c.JSON(fiber.Map{"status": 201, "message": "Image uploaded successfully", "data": data})
+	if err != nil {
+		log.Println("Error in uploading Image : ", err)
+		return c.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
+
+	}
+
+	uniqueId := uuid.New()
+
+	filename := strings.Replace(uniqueId.String(), "-", "", -1)
+
+	fileExt := strings.Split(file.Filename, ".")[1]
+
+	image := fmt.Sprintf("%s.%s", filename, fileExt)
+
+	err = c.SaveFile(file, fmt.Sprintf("./images/%s", image))
+
+	if err != nil {
+		log.Println("Error in saving Image :", err)
+		return c.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
+	}
+
+	imageUrl := fmt.Sprintf("http://localhost:3000/images/%s", image)
+
+	data := map[string]interface{}{
+
+		"imageName": image,
+		"imageUrl":  imageUrl,
+		"header":    file.Header,
+		"size":      file.Size,
+	}
+
+	return c.JSON(fiber.Map{"status": 201, "message": "Image uploaded successfully", "data": data})
 }
 
 func main() {
-    // Create a new Fiber app
-    app := fiber.New()
+	// Create a new Fiber app
+	app := fiber.New()
 
-    // Define a route for handling image uploads
-    app.Post("/upload", uploadImage)
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+	// Define a route for handling image uploads
+	app.Post("/upload", uploadImage)
 
-    // Start the Fiber server on port 3000
-    if err := app.Listen(":3000"); err != nil {
-        log.Fatalf("Error starting server: %v", err)
-    }
+	// Start the Fiber server on port 3000
+	if err := app.Listen(":3000"); err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
